c3630228be810976404d4bbdaf35906046dab3f6
ismail musa saidu <39607205+ismailmusa@users.noreply.github.com>
2022-08-07 14:12:17 +0100

    uploading project files

---
+++ upload_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=ismail1998/project-4
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login -u ismail1998
+docker tag project-4 $dockerpath
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
